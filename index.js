da88c16babc20fe189f9bf0ab446e812d72eed64
Agostina Gil <agostinagil973@gmail.com>
2023-12-27 11:47:35 +0100

    coding functions inBetween and notInBetween

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const d = document;
 const $playerBoard = d.querySelector(".player-cards"),
   $dealerBoard = d.querySelector(".dealer-cards"),
   $playBtn = d.querySelector(".play"),
-  $intermediateBtn = d.querySelector(".intermediate"),
-  $notIntermediateBtn = d.querySelector(".notIntermediate"),
+  $inBetweenBtn = d.querySelector(".inBetween"),
+  $notInBetweenBtn = d.querySelector(".notInBetween"),
   $newGameBtn = d.querySelector(".newGame"),
   $results = d.querySelector(".results");
 
@@ -14,6 +14,34 @@ d.addEventListener("click", (e) => {
     shuffle();
     startGame();
     $playBtn.classList.add("hidden");
+  }
+  if (e.target == $inBetweenBtn) {
+    inBetween();
+    $inBetweenBtn.disabled = true;
+    $notInBetweenBtn.disabled = true;
+  }
+
+  if (e.target == $notInBetweenBtn) {
+    notInBetween();
+    $inBetweenBtn.disabled = true;
+    $notInBetweenBtn.disabled = true;
+  }
+
+  if (e.target == $newGameBtn) {
+    console.clear();
+    createDeck();
+    shuffle();
+    $playerBoard.textContent = "";
+    $dealerBoard.textContent = "";
+    $results.textContent = "";
+    playerCards = [];
+
+    $inBetweenBtn.disabled = true;
+    $notInBetweenBtn.disabled = true;
+
+    setTimeout(() => {
+      startGame();
+    }, 1000);
   }
 });
 
@@ -40,28 +68,25 @@ const shuffle = () => {
 };
 
 const startGame = () => {
-  let hiddenCard = deck.pop();
   let $backCard = d.createElement("img");
   $backCard.src = `./cards/BACK.png`;
   $backCard.classList.add("card");
   $dealerBoard.append($backCard);
-  //   console.log("hidden card", hiddenCard);
+  // console.log("hidden card", hiddenCard);
 
   for (let i = 0; i < 2; i++) {
     let card = deck.pop();
-    let data = card.split("-");
-    let value = data[0].trim();
-    console.log(`CARD: ${card}, DATA: ${data}, VALUE: ${value}`);
+    let value = getValue(card);
     playerCards.push(value);
     let $playerCard = d.createElement("img");
     $playerCard.src = `./cards/${card}.png`;
     $playerCard.classList.add("card");
     $playerBoard.append($playerCard);
-    console.log(playerCards);
+    // console.log(playerCards);
   }
 
-  $intermediateBtn.disabled = false;
-  $notIntermediateBtn.disabled = false;
+  $inBetweenBtn.disabled = false;
+  $notInBetweenBtn.disabled = false;
 };
 
 const getValue = (card) => {
@@ -75,16 +100,67 @@ const getValue = (card) => {
   }
 };
 
+const inBetween = () => {
+  let hiddenCard = deck.pop();
+  let $thirdCard = d.createElement("img");
+  $thirdCard.src = `./cards/${hiddenCard}.png`;
+  $thirdCard.classList.add("card");
+  $dealerBoard.append($thirdCard);
+  let data = hiddenCard.split("-");
+  let value = data[0].trim();
+  let checkedValue = getValue(value);
+  playerCards.sort((a, b) => a - b);
+  console.log(playerCards);
+
+  let num = parseInt(checkedValue);
+  console.log(num);
+
+  if (playerCards.includes(checkedValue)) {
+    $results.textContent = `El valor (${checkedValue}) de la tercer carta es igual a un valor de tus cartas`;
+    console.log("no es intermedio");
+  }
+  if (num > playerCards[0] && num < playerCards[1]) {
+    $results.textContent = `El valor (${checkedValue}) de la tercer carta es intermedio entre tus dos cartas. Felicitaciones!`;
+    console.log(`La carta ${num} es intermedio`);
+  } else {
+    $results.textContent = `El valor (${checkedValue}) de la tercer carta no es intermedio entre tus dos cartas. Perdiste!`;
+  }
+};
+
+const notInBetween = () => {
+  let hiddenCard = deck.pop();
+  let $thirdCard = d.createElement("img");
+  $thirdCard.src = `./cards/${hiddenCard}.png`;
+  $thirdCard.classList.add("card");
+  $dealerBoard.append($thirdCard);
+  let data = hiddenCard.split("-");
+  let value = data[0].trim();
+  let checkedValue = getValue(value);
+  playerCards.sort((a, b) => a - b);
+  console.log(playerCards);
+
+  let num = parseInt(checkedValue);
+  console.log(num);
+
+  if (playerCards.includes(checkedValue)) {
+    $results.textContent = `El valor (${checkedValue}) de la tercer carta es igual a un valor de tus cartas. Ganaste`;
+    console.log("no es intermedio");
+  }
+  if (num > playerCards[0] && num < playerCards[1]) {
+    $results.textContent = `El valor (${checkedValue}) de la tercer carta es intermedio entre tus dos cartas. Perdiste!`;
+    console.log(`La carta ${num} es intermedio`);
+  } else {
+    $results.textContent = `El valor (${checkedValue}) de la tercer carta no es intermedio entre tus dos cartas. Ganaste!`;
+  }
+};
+
 /*
    Por hacer: 
-   - función para ver si hiddenCard está entre las cartas del jugador. 
-   - si el jugador elige que será intermedia o no, se ejecute la función que determina si la carta es intermedia o no y que muestre el mensaje correspondiente.
    - una vez ejecutada la función, que la carta "BACK" se oculte y que se muestre la hiddenCard. 
-   - que se muestre el mensaje correspondiente
+
+   - ver de unificar el codigo de las funciones inbetween y notinbetween para no repetir código, pero que se muestren mensajes distintos
 
    Idea función:
-   - ver método que extraiga los valores del array playerCards y que los ordene de mayor a menor. 
-   - ver cómo determinar si el valor de hiddenCard está entre los dos valores del array.
-   - tiene que devolver el mensaje correspondiente.
+
 
 */
